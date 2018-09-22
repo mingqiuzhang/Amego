3ab096c5e77fb72af3d0ef2ba62aad26e1a075fd
mingqiuzhang <35543135+mingqiuzhang@users.noreply.github.com>
2018-09-22 15:30:56 -0600

    Change Map done

--- Amigos/Assets/Scripts/LoadScene.cs
+++ Amigos/Assets/Scripts/LoadScene.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour {
+public class LoadScene : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
 }
