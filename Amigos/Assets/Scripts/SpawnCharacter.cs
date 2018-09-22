195e9c4fe3a308ec3c44bab4f3e294cf39110348
mingqiuzhang <35543135+mingqiuzhang@users.noreply.github.com>
2018-09-22 15:06:41 -0600

    Added some scripts
    Added Start, Pause, Quit, Restart, and Spawn scripts. The change map one is not done yet.

---
+++ Amigos/Assets/Scripts/SpawnCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour {
+    public Transform[] spawnLocations;
+    public GameObject[] spawnPrefeb;
+    public GameObject[] spawnClone;
+
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        spawnClone[0] = Instantiate(spawnPrefeb[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+        spawnClone[1] = Instantiate(spawnPrefeb[1], spawnLocations[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        spawnClone[2] = Instantiate(spawnPrefeb[2], spawnLocations[2].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        spawnClone[3] = Instantiate(spawnPrefeb[3], spawnLocations[3].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+
+}
