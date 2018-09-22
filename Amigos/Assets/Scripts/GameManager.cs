70dee1327fd19e294411da62a751e1b6d77f858e
mingqiuzhang <35543135+mingqiuzhang@users.noreply.github.com>
2018-09-22 15:44:01 -0600

    Added GameManager script
    GameManager controls the scenes and tracks the wins.

---
+++ Amigos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] players;
+
+    void Update()
+    {
+        if (players.Length == 1)
+        {
+            if (players[0].GetComponent<Player>().WinCounts == 3)
+            {
+                SceneLoader(4);
+            }
+            SceneLoader(Random.Range(1, 3));
+        }
+    }
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
