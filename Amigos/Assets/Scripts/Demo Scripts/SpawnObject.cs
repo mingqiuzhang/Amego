d08ee38d66ea1a99ca565a21b9c99ac156342714
Pix Wang <35542063+w853245475@users.noreply.github.com>
2018-09-22 19:33:49 -0600

    We Got A Prefeb Problem!

--- Amigos/Assets/Scripts/Demo Scripts/SpawnObject.cs
+++ Amigos/Assets/Scripts/Demo Scripts/SpawnObject.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class SpawnCharacter : MonoBehaviour {
+public class SpawnObject : MonoBehaviour {
     public Transform[] spawnLocations;
     public GameObject[] spawnPrefeb;
-    public GameObject[] spawnClone;
+    //public GameObject[] spawnClone;
 
     void Start()
     {
@@ -14,10 +15,13 @@ public class SpawnCharacter : MonoBehaviour {
 
     void Spawn()
     {
-        spawnClone[0] = Instantiate(spawnPrefeb[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
-        spawnClone[1] = Instantiate(spawnPrefeb[1], spawnLocations[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
-        spawnClone[2] = Instantiate(spawnPrefeb[2], spawnLocations[2].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
-        spawnClone[3] = Instantiate(spawnPrefeb[3], spawnLocations[3].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        //spawnClone[0] = Instantiate(spawnPrefeb[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+        //spawnClone[1] = Instantiate(spawnPrefeb[1], spawnLocations[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        //spawnClone[2] = Instantiate(spawnPrefeb[2], spawnLocations[2].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        //spawnClone[3] = Instantiate(spawnPrefeb[3], spawnLocations[3].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        PrefabUtility.InstantiatePrefab(spawnPrefeb[0].gameObject as GameObject);
+        PrefabUtility.InstantiatePrefab(spawnPrefeb[1].gameObject as GameObject);
+
     }
 
 }
