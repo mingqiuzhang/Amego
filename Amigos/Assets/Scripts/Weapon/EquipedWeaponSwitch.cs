bb72e85baac1b3af6009c116fed0b0ff0005c11d
pengxi wang <w853245475@gmail.com>
2018-10-09 16:24:49 -0600

    2018/10/9
    2018/10/9
    Created New Scripts
    Switch Weapons by Player Colliding is Avaliable Now.

---
+++ Amigos/Assets/Scripts/Weapon/EquipedWeaponSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipedWeaponSwitch : MonoBehaviour {
+
+    public GameObject[] current;
+	// Use this for initialization
+	void Start () {
+	}
+
+    void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.gameObject.tag == "weapon" || _collider.gameObject.tag == "bow")
+        {
+            foreach (GameObject i in current)
+            {
+                if (i.active && i.name != _collider.name)
+                {
+                    i.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Amigos/Assets/Scripts/Weapon/PickUpWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpWeapon : MonoBehaviour {
+    public GameObject myWeapon;
+    public GameObject groundWeapon;
+	// Use this for initialization
+	void Start () {
+        myWeapon.SetActive(false);
+	}
+
+    void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.gameObject.tag == "Player")
+        {
+            myWeapon.SetActive(true);
+            groundWeapon.SetActive(false);
+        }
+    }
+}
