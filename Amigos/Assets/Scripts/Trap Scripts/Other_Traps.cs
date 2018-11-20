90c86d0049e13fb1a5b9b1ea1defeb01dda2c145
Pengxi Wang <35542063+w853245475@users.noreply.github.com>
2018-11-19 18:37:37 -0700

    2018/11/19
    Scripts for pillar to kill characters

---
+++ Amigos/Assets/Scripts/Trap Scripts/Other_Traps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Other_Traps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //This Part Is To Set The Character Kinematic.
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player" || other.tag == "Player2" || other.tag == "Player3" || other.tag == "Player4")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
