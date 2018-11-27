d0e54960916f51a9d201c1929b3bad5a176b1aa9
pengxi wang <w853245475@gmail.com>
2018-11-26 19:41:40 -0700

    new

--- Amigos/Assets/Scripts/Animation/AnimationHandler.cs
+++ Amigos/Assets/Scripts/Animation/AnimationHandler.cs
@@ -10,6 +10,16 @@ public class AnimationHandler : MonoBehaviour {
         _playerAnimator = this.GetComponent<Animator>();
         _character = this.GetComponent<CharacterMovement_Physics>();
     }
+
+    public void StartSwordAttack()
+    {
+        print("Sword dealing damage now");
+    }
+
+    public void EndSwordAttack()
+    {
+        print("Sword not dealing damage now");
+    }
 	
     public void PlayAttackAnimation(EquipedWeaponSwitch.CurrentWeapon i_weapon)
     {
